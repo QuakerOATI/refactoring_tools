5a7a96f0c4135aa34218ae391d0e3553337eb922
Michael Haynes <Michael.Haynes@oati.net>
2023-11-22 10:43:14 -0600

    Implement from_format_string functionality

--- src/format_specifier.py
+++ src/format_specifier.py
@@ -1,204 +1,35 @@
+from __future__ import annotations
 import string
 import dataclasses
-from abc import ABC
-from __future__ import annotations
-from typing import List, Callable, Tuple, Literal, TypeVar, Union
-from collections import deque
-from contextlib import AbstractContextManager
-from functools import partial
+from typing import List, Callable, Tuple, Literal, TypeVar, Union, Any, Optional
+from exception_stack import ExceptionStack
 
-NUMERIC_TYPES = [
-    "decimal",
-    "integer",
-    "unsigned_integer",
-    "hex_lowercase",
-    "hex_uppercase",
-    "octal",
-    "fixed_point_lowercase",
-    "fixed_point_uppercase",
-    "general_format_lowercase",
-    "general_format_uppercase",
-    "binary",
-    "percentage",
-    "decimal_localized",
-]
-
-NONNUMERIC_TYPES = [
-    "string",
-    "repr",
-    "ascii",
-    "char",
-]
-
-PERCENT_FORMAT_OPTS = {
-    "align": {
-        "": "",
-        "left": "-",
-        "right": "",
-    },
-    "numeric_flags": {
-        "": "",
-        "alternate_form": "#",
-        "pad_zeros": "0",
-        "negative_signed": "",
-        "always_signed": "+",
-        "negative_signed_aligned": " ",
-    },
-    "type": {
-        "": "s",
-        "decimal": "d",
-        "integer": "i",
-        "unsigned_integer": "u",
-        "hex_lowercase": "x",
-        "hex_uppercase": "X",
-        "octal": "o",
-        "fixed_point_lowercase": "f",
-        "fixed_point_uppercase": "F",
-        "scientific_lowercase": "e",
-        "scientific_uppercase": "E",
-        "general_format_lowercase": "g",
-        "general_format_uppercase": "G",
-        "character": "c",
-        "string": "s",
-        "repr": "r",
-        "ascii": "a",
-    },
+NUMERIC_TYPES = {
+    "decimal": "d",
+    "integer": "i",
+    "unsigned_integer": "u",
+    "hex_lowercase": "x",
+    "hex_uppercase": "X",
+    "octal": "o",
+    "fixed_point_lowercase": "f",
+    "fixed_point_uppercase": "F",
+    "general_format_lowercase": "g",
+    "general_format_uppercase": "G",
+    "binary": "b",
+    "percentage": "%",
+    "decimal_localized": "n",
 }
 
-BRACE_FORMAT_OPTS = {
-    "align": {
-        "": "",
-        "left": "<",
-        "right": ">",
-        "center": "^",
-        "sign_on_left": "=",
-    },
-    "numeric_flags": {
-        "": "",
-        "alternate_form": "#",
-        "pad_zeros": "0",
-        "float_positive_zero": "z",
-        "group_commas": ",",
-        "group_underscores": "_",
-        "negative_signed": "-",
-        "always_signed": "+",
-        "negative_signed_aligned": " ",
-    },
-    "type": {
-        "": "s",
-        "binary": ("", "b"),
-        "decimal": ("", "d"),
-        "integer": ("", "i"),
-        "hex_lowercase": ("", "x"),
-        "hex_uppercase": ("", "X"),
-        "octal": ("", "o"),
-        "fixed_point_lowercase": ("", "f"),
-        "fixed_point_uppercase": ("", "F"),
-        "scientific_lowercase": ("", "e"),
-        "scientific_uppercase": ("", "E"),
-        "general_format_lowercase": ("", "g"),
-        "general_format_uppercase": ("", "G"),
-        "character": ("", "c"),
-        "string": ("s", "s"),
-        "repr": ("r", "s"),
-        "ascii": ("a", "s"),
-        "percentage": ("", "%"),
-        "decimal_localized": ("", "n"),
-    },
+NUMERIC_TYPES_INV = {v: k for k, v in NUMERIC_TYPES.items()}
+
+NONNUMERIC_TYPES = {
+    "string": "s",
+    "repr": "r",
+    "ascii": "a",
+    "char": "c",
 }
 
-
-class ExceptionStack(AbstractContextManager):
-    """Accumulate exceptions over a series of tasks.
-
-    Methods:
-        join: execute tasks and return results, aggregating any exceptions into
-            an ExceptionGroup
-        map: extend the list of tasks by mapping a function over a list of
-            argument tuples
-    """
-
-    def __init__(self, tasks: List[Callable[[], Any]] = []) -> None:
-        """ExceptionGroup constructor.
-
-        Args:
-            tasks: list of callables to be executed (additional tasks can be
-                added after initialization)
-        """
-        self.tasks = deque(tasks)
-        self.exceptions = []
-
-    def join(self) -> List[Any]:
-        """Execute pending tasks and return the list of return values.
-
-        Tasks that fail to return correspond to values of None in the list of
-        results.
-
-        Exceptions thrown by individual tasks are caught, annotated with the
-        index of self.tasks at which they were raised, and appended to
-        self.exceptions.
-
-        This method is wrapped by ExceptionStack.__enter__, allowing it to be
-        used as a context manager.  For example:
-
-        >>> with ExceptionStack(tasks) as results:
-        >>>     # do something with results
-        >>>     # all tasks are guaranteed to execute
-
-        When using an ExceptionStack this way, self.exceptions is aggregated
-        into an ExceptionGroup and reraised upon exiting the context.
-
-        Returns:
-            list of values returned by tasks in self.tasks
-        """
-        results = []
-        while self.tasks:
-            task = self.tasks.popleft()
-            try:
-                results.append(task())
-            except Exception as e:
-                e.add_note(
-                    f"Exception occurred in task index {len(results)} of Exception Stack"
-                )
-                self.exceptions.append(e)
-                results.append(None)
-        return results
-
-    def resolve(self) -> None:
-        """Resolve exceptions by combining into an ExceptionGroup and raising.
-
-        This method is called by ExceptionStack.__exit__, so it is not
-        necessary to call it again when using ExceptionStack as a context
-        manager.
-
-        Raises:
-            ExceptionGroup
-        """
-        if self.exceptions:
-            raise ExceptionGroup("Exception stack terminated with errors",
-                                 self.exceptions) from None
-
-    def map(self, func: Callable[[...], Any],
-            args: List[Tuple[Any]]) -> ExceptionStack:
-        """Add tasks to self by mapping a function over a list of tuples.
-
-        Args:
-            func: function to be mapped
-            args: list of tuples to which func should be applied
-        Returns:
-            self (to allow method-chaining)
-        """
-        self.tasks.extend([partial(func, *a) for a in args])
-        return self
-
-    def __enter__(self):
-        return self.join()
-
-    def __exit__(self, exc_type, exc_val, traceback):
-        # If an exceptions is raised outside of individual tasks, append it here
-        if isinstance(exc_val, Exception):
-            self.exceptions.append(exc_val)
-        self.resolve()
+NONNUMERIC_TYPES_INV = {v: k for k, v in NONNUMERIC_TYPES.items()}
 
 
 @dataclasses.dataclass
@@ -207,8 +38,9 @@ class NumericFormat:
     pad_zeros: bool = False
     float_positive_zero: Optional[bool] = False
     group: Optional[Literal["comma", "underscore"]] = None
-    sign: Literal["sign_positive", "nosign_positive",
-                  "align_positive"] = "nosign_positive"
+    sign: Literal[
+        "sign_positive", "nosign_positive", "align_positive"
+    ] = "nosign_positive"
     justifySign: Optional[bool] = None
     precision: Optional[int] = None
 
@@ -225,37 +57,57 @@ NonnumericType: TypeVar = Literal[*NONNUMERIC_TYPES]
 TypeFormat: TypeVar = Union[NumericType, NonnumericType]
 
 
-class FormatSpecifier(ABC):
-
-    def __init__(self, alignmentFormat: AlignmentFormat,
-                 numericFormat: NumericFormat, typeFormat: TypeFormat) -> None:
+class FormatSpecifier:
+    def __init__(
+        self,
+        alignmentFormat: AlignmentFormat,
+        numericFormat: NumericFormat,
+        typeFormat: TypeFormat,
+        fieldName: Optional[str] = None,
+    ) -> None:
         self.alignmentFormat = alignmentFormat
         self.numericFormat = numericFormat
         self.typeFormat = typeFormat
+        self.fieldName = fieldName
         self.validate()
 
     def validate_typeFormat(self):
         if self.typeFormat in NONNUMERIC_TYPES:
-            assert self.numericFormat == NumericFormat(
+            assert (
+                self.numericFormat == NumericFormat()
             ), "Numeric format options cannot be specified for non-numeric types"
         else:
-            assert self.numericFormat in NUMERIC_TYPES, f"Invalid type format '{self.numericFormat}'"
+            assert (
+                self.typeFormat in NUMERIC_TYPES
+            ), f"Invalid type format '{self.typeFormat}'"
 
     def validate(self) -> None:
         validation_methods = [
-            attr for name in dir(self) if callable(
-                attr := getattr(self, name)) and name.startswith("validate_")
+            attr
+            for name in dir(self)
+            if callable(attr := getattr(self, name)) and name.startswith("validate_")
         ]
         with ExceptionStack(validation_methods):
             pass
 
-    @abstractmethod
+    @classmethod
+    def from_spec(cls, specifier: FormatSpecifier):
+        return cls(
+            alignmentFormat=specifier.alignmentFormat,
+            numericFormat=specifier.numericFormat,
+            typeFormat=specifier.typeFormat,
+            fieldName=specifier.fieldName,
+        )
+
+    @classmethod
+    def from_text_fields(cls, fields: List[Union[str, FormatSpecifier]]):
+        return "".join(list(map(str, fields)))
+
     def __repr__(self):
-        ...
+        return f"FormatSpecifier(alignment: {self.alignmentFormat}, numeric: {self.numericFormat}, type: {self.typeFormat}, field: {self.fieldName})"
 
 
 class PercentFormatSpecifier(FormatSpecifier):
-
     JUSTIFY = {
         "left": "-",
         "right": "",
@@ -263,7 +115,14 @@ class PercentFormatSpecifier(FormatSpecifier):
 
     SIGN = {"sign_positive": "+", "nosign_positive": "", "align_positive": " "}
 
-    def __repr__(self):
+    TYPE_UNDEF = ["decimal_localized", "percentage", "binary"]
+
+    def validate_type_defined(self):
+        assert (
+            self.typeFormat not in self.TYPE_UNDEF
+        ), f"Type '{self.typeFormat}' not defined for percent-style format specifiers"
+
+    def __str__(self):
         fmt = self.SIGN[self.numericFormat.sign]
         fmt += self.JUSTIFY[self.alignmentFormat.justify]
         if self.numericFormat.pad_zeros:
@@ -274,12 +133,14 @@ class PercentFormatSpecifier(FormatSpecifier):
             fmt += str(self.alignmentFormat.width)
         if self.numericFormat.precision:
             fmt += f".{self.numericFormat.precision}"
-        fmt += self.typeFormat
-        return f"%{fmt}"
+        fmt += (NUMERIC_TYPES | NONNUMERIC_TYPES).get(self.typeFormat)
+        field_spec = "%"
+        if field := self.fieldName:
+            field_spec += f"({field})"
+        return f"{field_spec}{fmt}"
 
 
 class BraceFormatSpecifier(FormatSpecifier):
-
     JUSTIFY = {
         "left": "<",
         "right": ">",
@@ -294,10 +155,23 @@ class BraceFormatSpecifier(FormatSpecifier):
 
     GROUP = {"comma": ",", "underscore": "_"}
 
-    def __repr__(self):
-        fmt += self.SIGN[self.numericFormat.sign]
+    TYPE_UNDEF = ["char"]
+
+    def validate_type_defined(self):
+        assert (
+            self.typeFormat not in self.TYPE_UNDEF
+        ), f"Type '{self.typeFormat}' not defined for brace-style format specifiers"
+
+    def validate_group(self):
+        if self.typeFormat == "decimal_localized":
+            assert (
+                self.numericFormat.group is None
+            ), "Cannot specify a group separator with numeric type '{self.typeFormat}'"
+
+    def __str__(self):
         fmt = self.JUSTIFY[self.alignmentFormat.justify]
-        if self.alignmentFormat.fill:
+        fmt += self.SIGN[self.numericFormat.sign]
+        if fill := self.alignmentFormat.fill:
             fmt = str(fill)[0] + fmt
         if self.numericFormat.float_positive_zero:
             fmt += "z"
@@ -305,11 +179,60 @@ class BraceFormatSpecifier(FormatSpecifier):
             fmt += "#"
         if self.numericFormat.pad_zeros:
             fmt += "0"
-        if self.alignmentFormat.width:
-            fmt += str(self.alignmentFormat.width)
-        if self.numericFormat.group:
-            fmt += self.GROUP[self.numericFormat.group]
-        if self.numericFormat.precision:
-            fmt += f".{self.numericFormat.precision}"
-        fmt += self.typeFormat
+        if width := self.alignmentFormat.width:
+            fmt += str(width)
+        if group := self.numericFormat.group:
+            fmt += self.GROUP[group]
+        if precision := self.numericFormat.precision:
+            fmt += f".{precision}"
+        if self.typeFormat in NONNUMERIC_TYPES:
+            fmt = f"!{NONNUMERIC_TYPES[self.typeFormat]}:{fmt}s"
+        else:
+            fmt = f":{fmt}{NUMERIC_TYPES[self.typeFormat]}"
+        if field := self.fieldName:
+            fmt = f"{field}{fmt}"
         return "{" + fmt + "}"
+
+    @classmethod
+    def _from_formatter_parser_field(cls, field: str):
+        text, fieldName, spec, conv = field
+        if spec is None:
+            return text
+        num = NumericFormat()
+        align = AlignmentFormat()
+        if not conv:
+            if spec[-1].isalpha():
+                conv = spec[-1]
+            else:
+                conv = "s"
+        typeName = (NUMERIC_TYPES_INV | NONNUMERIC_TYPES_INV)[conv]
+        for k, v in cls.JUSTIFY.items():
+            if v in spec:
+                align.justify = k
+                if (idx := spec.find(v)) > 0:
+                    align.fill = spec[:idx]
+        for k, v in cls.SIGN.items():
+            if v in spec:
+                num.sign = k
+        for k, v in cls.GROUP.items():
+            if v in spec:
+                num.group = k
+        if "z" in spec:
+            num.float_positive_zero = True
+        if "#" in spec:
+            num.alternate_form = True
+        if "." in spec:
+            num.precision = int(
+                "".join([c for c in spec[spec.find(".") :] if c.isnumeric()])
+            )
+        width = "".join([c for c in spec[: spec.find(".")] if c.isnumeric()])
+        if width.startswith("0"):
+            num.pad_zeros = True
+        if width:
+            align.width = int(width)
+        return FormatSpecifier(align, num, typeName, fieldName)
+
+    @classmethod
+    def from_format_string(cls, fmt: str) -> List[Union[str, FormatSpecifier]]:
+        fields = string._string.formatter_parser(fmt)
+        return [cls._from_formatter_parser_field(field) for field in fields]
