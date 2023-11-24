b469cba292388d9461855b9e0abf906e6da41938
Michael Haynes <Michael.Haynes@oati.net>
2023-11-24 10:42:18 -0600

    Fix and test: ReplaceEprintWithLogger

--- src/transformers/replace_function.py
+++ src/transformers/replace_function.py
@@ -44,52 +44,26 @@ Ideas:
 """
 
 
-class LastArgument(
-    cst.metadata.BatchableMetadataProvider[Optional[cst.BaseExpression]]
-):
-    """Marks function calls with the value of their last argument."""
-
-    def visit_Call(self, node: cst.Call) -> None:
-        if node.args:
-            self.set_metadata(node, node.args[-1].value)
-        else:
-            self.set_metadata(node, None)
-
-
-class IsFirstArgument(cst.metadata.BatchableMetadataProvider[bool]):
-    """Marks the first argument of each function call."""
-
-    def visit_Call(self, node: cst.Call) -> None:
-        if node.args:
-            self.set_metadata(node.args[0], True)
-
-
-class IsLogLevelLiteral(cst.metadata.BatchableMetadataProvider[bool]):
-    """Marks the string literals corresponding to named logging levels."""
-
-    def visit_SimpleString(self, node: cst.SimpleString) -> None:
-        self.set_metadata(
-            node,
-            node.value in ["DEBUG", "INFO", "WARN", "WARNING", "ERROR", "CRITICAL"],
-        )
-
-
 LOGLEVELS = ["DEBUG", "INFO", "WARN", "WARNING", "ERROR", "CRITICAL", "FATAL"]
 
 
 def LogLevelLiteral():
-    return m.SimpleString(value=m.matches(lambda value: value in LOGLEVELS))
+    """Factory for use with libcst.matchers functions and decorators"""
+    # Note that SimpleString.value includes the quotes, so we have to check value[1:-1]
+    return m.SimpleString(value=m.MatchIfTrue(lambda value: value[1:-1] in LOGLEVELS))
 
 
 def TemplateString():
-    return m.Call(func=m.Attribute(value=m.SimpleString, attr=m.Name(value="format")))
+    """Factory for use with libcst.matchers functions and decorators"""
+    return m.Call(func=m.Attribute(value=m.SimpleString(), attr=m.Name(value="format")))
 
 
 def EprintStatement():
+    """Factory for use with libcst.matchers functions and decorators"""
     return m.Call(
         func=m.Name(value="eprint"),
         args=m.MatchIfTrue(
-            lambda args: args and m.matches(args[-1], LogLevelLiteral())
+            lambda args: args and m.matches(args[-1].value, LogLevelLiteral())
         ),
     )
 
@@ -97,37 +71,23 @@ def EprintStatement():
 class ReplaceEprintWithLogger(m.MatcherDecoratableTransformer):
     @m.leave(EprintStatement())
     def change_eprint_to_logger(self, original: cst.Call, updated: cst.Call):
-        args = updated.args[:-1]
-        fmt = args[0]
-        if m.matches(TemplateString(), fmt):
-            spec = ...
-        if m.matches(last_arg := updated.args[-1], m.Arg(value=m.SimpleString())):
-            if (loglevel := last_arg.value) in LOGLEVELS:
-                return updated.with_changes(
-                    func=cst.Attribute(
-                        value=cst.Name(value="logger"),
-                        attr=cst.Name(value=loglevel.lower()),
-                    ),
-                    args=[
-                        cst.Arg(value=cst.SimpleString(value=printf_template)),
-                        *printf_args,
-                    ],
-                )
-
-    @m.call_if_inside(EprintStatement())
-    @m.visit(TemplateString())
-    def unroll_formatted_string(self, node: cst.Call) -> None:
-        fmt = cst.Arg(node.func.value)
-        return cst.List([fmt, *node.func.args])
-
-
-class IsFormatString(cst.metadata.BatchableMetadataProvider[bool]):
-    """Marks calls to .format(...) on strings literal."""
-
-    def visit_SimpleString(self, node: cst.SimpleString) -> None:
-        ...
-
-
-class ConvertBraceFormatToPercent(m.MatcherDecoratableTransformer):
-    def update_function(self, original: cst.Call, updated: cst.Call):
-        ...
+        fmt = updated.args[0].value
+        method = updated.args[-1].value.value[1:-1]
+        args = updated.args[1:-1]
+        if m.matches(fmt, m.SimpleString()):
+            fmt = fmt.value
+        elif m.matches(fmt, TemplateString()):
+            args = fmt.args + args
+            fmt = fmt.func.value.value.replace("{}", "%s")
+        else:
+            raise ValueError(f"Unknown format for first eprint argument: {fmt}")
+        return updated.with_changes(
+            func=cst.Attribute(
+                value=cst.Name(value="logger"),
+                attr=cst.Name(method.lower()),
+            ),
+            args=[
+                cst.Arg(value=cst.SimpleString(value=fmt)),
+                *args,
+            ],
+        )
