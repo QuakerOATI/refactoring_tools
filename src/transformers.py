93bd05c4ceb1044f6c978173cfb4f81519119074
Michael Haynes <Michael.Haynes@oati.net>
2023-11-27 11:12:58 -0600

    Implement RemoveEprint

--- src/transformers.py
+++ src/transformers.py
@@ -3,52 +3,16 @@ import libcst.matchers as m
 from libcst.helpers import get_full_name_for_node
 from .matchers import EprintStatement, TemplateString
 from .metadata import IsCaughtException
-from typing import List
+from typing import List, Union
 
-"""
-1. Rename eprint --> logger method
-2. Reconstruct arguments to eprint/logger method
-3. When in a catch block, use the logger.exception method
-    - only when eprint statement has an argument equal to the target of an "Exception as" assignment
-
-Needed components:
-    * "name is exception" provider
-        * matcher for except blocks
-    * matcher for first and last args to function call
-    * matcher for string.format() expressions
-    * transformer for format --> printf conversion
-    * IsLoggerStatement matcher
-
-Existing components:
-    * cst.Raise
-        - includes attribute for optional "from" clase
-    * cst.Try
-        - cannot contain TryStar blocks
-        - Try.handlers
-    * cst.ExceptHandler
-        - handler.body
-        - handler.type: Tuple[...] or None
-        - name --> name of caught exception
-
-Notes:
-    * can match on number of arguments to Call
-    * declare metadata providers with METADATA_DEPENDENCIES class attribute
-        * metadata is then accessed via self.get_metadata(provider_type, node)
-    * providers should extend BatchableMetadataProvider
-    * ParentNodeProvider can be used for backtracking
-
-Ideas:
-    * cst.metadata.QualifiedNameProvider/FullyQualifiedNameProvider:
-        - use to automate import restructuring
-    * match.MatchIfTrue:
-        - match name against predefined list
-
-"""
+"""Transformers for refactoring the webSmartForecast codebase."""
 
 
 def ExceptionHandlerLoggingStatement(
     function_context: List[str], logger_name="logger"
 ) -> cst.Call:
+    """Factory for a libcst node representing a logger.exception call."""
+
     return cst.Call(
         func=cst.Attribute(
             val=cst.Name(logger_name),
@@ -62,6 +26,8 @@ def ExceptionHandlerLoggingStatement(
 
 
 class FunctionContextMixin(m.MatcherDecoratableTransformer):
+    """Mixin for a transformer that maintains nested-function context."""
+
     def __init__(self) -> None:
         self.function_context = []
 
@@ -77,10 +43,42 @@ class FunctionContextMixin(m.MatcherDecoratableTransformer):
         return updated
 
 
+class RemoveEprintDef(m.MatcherDecoratableVisitor):
+    """Remove function defs of eprint."""
+
+    @m.leave(m.FunctionDef(name=m.Name("eprint")))
+    def remove_func(
+        self, original: cst.FunctionDef, updated: cst.FunctionDef
+    ) -> Union[cst.FunctionDef, cst.RemovalSentinel]:
+        return cst.RemoveFromParent()
+
+
 class ReplaceEprintWithLogger(FunctionContextMixin):
+    """Replace all eprint calls with logger calls.
+
+    If the eprint call occurs inside an `except` block and at least one of its
+    arguments is a caught exception, it will be replaced with a call to
+    `logger.exception` with the keyword argument exc_info=True. Otherwise, the
+    last argument will be taken as the name of the logger method to use, and
+    the first argument to be the template string of message.
+
+    The name of the logger object to use can be passed in the constructor.
+    """
+
     METADATA_DEPENDENCIES = (IsCaughtException,)
 
+    class EprintWarning(Warning):
+        pass
+
+    class EprintException(Exception):
+        pass
+
     def __init__(self, logger_name="logger") -> None:
+        """Constructor for ReplaceEprintWithLogger transformer.
+
+        Args:
+            logger_name: name of logger object to use in replacement
+        """
         super().__init__()
         self.excs_in_eprint = []
         self.logger_name = logger_name
@@ -102,18 +100,45 @@ class ReplaceEprintWithLogger(FunctionContextMixin):
         method = updated.args[-1].value.value[1:-1]
         args = updated.args[1:-1]
 
+        if args:
+            # If there are any additional args other than file, raise warning
+            # The file args can be safely ignored, since we can include that
+            # information by coniguring the root logger's formatter
+            if any(
+                map(
+                    m.matches(
+                        m.Arg(value=m.DoesNotMatch(m.Name("file") | m.Name("File"))),
+                        args,
+                    )
+                )
+            ):
+                raise self.EprintWarning(
+                    f"Unrecognized arguments in eprint call: {args}"
+                )
+
+        # Now that we've issued warnings about any anomalies, ignore all args
+        # other than loglevel and message
+        args = []
+
+        # If any args inside the eprint call reference an exception, assume we
+        # should replace the call with logger.exception(...)
         if self.excs_in_eprint.pop() > 0:
             return ExceptionHandlerLoggingStatement(
                 self.function_context, self.logger_name
             )
 
+        # First arg should be either a format string or a simple string
         if m.matches(fmt, m.SimpleString()):
             fmt = fmt.value
         elif m.matches(fmt, TemplateString()):
-            args = fmt.args + args
+            # The args we want are in the call to .format()
+            args = fmt.args
+            # Simpleminded, but Good Enough for this use case
             fmt = fmt.func.value.value.replace("{}", "%s")
         else:
-            raise ValueError(f"Unknown format for first eprint argument: {fmt}")
+            raise self.EprintException(
+                f"Unknown format for first eprint argument: {fmt}"
+            )
 
         return updated.with_changes(
             func=cst.Attribute(
